d754f9f09906ea9b2a0dc91dbdc9c685f4bc44ed
Luke McGuire <luke.mcguire@gmail.com>
2025-06-04 16:37:10 -0700

    fix: forgot to update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,5 +1,5 @@
 """LLM Functions Package."""
 
-__all__ = ["get_file_content", "get_files_info", "write_file"]
+__all__ = ["get_file_content", "get_files_info", "run_python_file", "write_file"]
 
-from . import get_file_content, get_files_info, write_file
+from . import get_file_content, get_files_info, run_python_file, write_file
